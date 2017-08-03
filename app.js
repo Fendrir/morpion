781c9570a649aca1efaf4ab8b8bacbde2d0e6bf5
Julien <papao1@hotmail.fr>
2017-08-03 12:13:16 +0200

    verification ne marche toujours pas et compteur aussi trouver autre solution

--- app.js
+++ app.js
@@ -44,21 +44,41 @@
 // });
 
 // ------------------------------ enregistrer les players ---------------------------
+var objet = {};
+
+// ------------ player 1 -----------------
+
 
 $("#valider_player_1").click (function() {
   	var joueur1 = $("#player_1").val();
+    objet.joueur1 = joueur1;
 
-$("#texte-player-1").append("bienvenue "+joueur1);
+$("#texte-player-1").html("bienvenue "+objet.joueur1);
 
  });
+
+$('#player_1').change(function(){
+    objet.joueur1 = $("#player_1").val();
+
+    $("#texte-player-1").html("bienvenue "+objet.joueur1);
+});
+
+//------------------------ Player 2 --------------------------------
 
 $("#valider_player_2").click (function() {
   	var joueur2 = $("#player_2").val();
+    objet.joueur2 = joueur2;
 
-$("#texte-player-2").html("bienvenue "+ joueur2);
+$("#texte-player-2").html("bienvenue "+ objet.joueur2);
 
  });
 
+
+$('#player_2').change(function(){
+    objet.joueur2 = $("#player_2").val();
+
+    $("#texte-player-2").html("bienvenue "+objet.joueur2);
+});
 // -------------------------- condition positionning tour par tour -------------------
 
 
@@ -72,86 +92,6 @@ $("#texte-player-2").html("bienvenue "+ joueur2);
 	  var fondBlanc = '<img class="fond-Blanc" src="images/fond-blanc.jpg">';
 
 
-	  // 	function changeImage(i){
-   //        if (i%2) {
-			// if ($("#case1").click(function() {
-          	
-			// 	$("#case1").append(nukeBomb);
-			// }) 
-			// 	|| $("#case2").click(function() {
-          	
-			// 	$("#case2").append(nukeBomb);
-			// }) 
-			// 	|| $("#case3").click(function() {
-          	
-			// 	$("#case3").append(nukeBomb);
-			// }) 
-			// 	|| $("#case4").click(function() {
-          	
-			// 	$("#case4").append(nukeBomb);
-			// }) 
-			// 	|| $("#case5").click(function() {
-          	
-			// 	$("#case5").append(nukeBomb);
-			// }) 
-			// 	|| $("#case6").click(function() {
-          	
-			// 	$("#case6").append(nukeBomb);
-			// }) 
-			// 	|| $("#case7").click(function() {
-          	
-			// 	$("#case7").append(nukeBomb);
-			// }) 
-			// 	|| $("#case8").click(function() {
-          	
-			// 	$("#case8").append(nukeBomb);
-			// }) 
-			// 	|| $("#case9").click(function() {
-          	
-			// 	$("#case9").append(nukeBomb);
-			// }));
-
-   //        } else {
-
-   //        	$("#case1").click(function() {
-          	
-			// 	$("#case1").append(bactBomb);
-			// }) 
-			// 	|| $("#case2").click(function() {
-          	
-			// 	$("#case2").append(bactBomb);
-			// }) 
-			// 	|| $("#case3").click(function() {
-          	
-			// 	$("#case3").append(bactBomb);
-			// }) 
-			// 	|| $("#case4").click(function() {
-          	
-			// 	$("#case4").append(bactBomb);
-			// }) 
-			// 	|| $("#case5").click(function() {
-          	
-			// 	$("#case5").append(bactBomb);
-			// }) 
-			// 	|| $("#case6").click(function() {
-          	
-			// 	$("#case6").append(bactBomb);
-			// }) 
-			// 	|| $("#case7").click(function() {
-          	
-			// 	$("#case7").append(bactBomb);
-			// }) 
-			// 	|| $("#case8").click(function() {
-          	
-			// 	$("#case8").append(bactBomb);
-			// }) 
-			// 	|| $("#case9").click(function() {
-          	
-			// 	$("#case9").append(bactBomb);
-			// })
-
-   //        }
-   //  };
 
 
 var joueur = 1;
@@ -166,31 +106,39 @@ $(".case").on("click",function donneImage(){
 		}
 });
 
+      // function donneImage(i)
+      // {
+      //     if (i%2)
+      //         return $(this).html(nukeBomb) ;
+      //     else
+      //         return $(this).html(bactBomb) ;
+      // }
 
-      function verifier(a, b, c)
-      {
-          if (   document.images[a].src == document.images[b].src
-              && document.images[b].src == document.images[c].src
-              && document.images[c].src != fondBlanc.src)
-          {
-              window.alert ('Nous avons un gagnant !') ;
-              return true ;
-          }
 
-          return false ;
-      }
+
+          // if (   document.images[a].src == document.images[b].src
+          //     && document.images[b].src == document.images[c].src
+          //     && document.images[c].src != fondBlanc.src)
+          // {
+          //     window.alert ('Nous avons un gagnant !') ;
+          //     // return true ;
+          // }
+
+      //     return false ;
+      // }
+
 
 
       function gagnant()
       {
-          return verifier($("#case1"), $("#case2"), $("#case3"))
-              || verifier($("#case4"), $("#case5"), $("#case6"))
-              || verifier($("#case7"), $("#case8"), $("#case9"))
-              || verifier($("#case1"), $("#case4"), $("#case7"))
-              || verifier($("#case2"), $("#case5"), $("#case8"))
-              || verifier($("#case3"), $("#case6"), $("#case9"))
-              || verifier($("#case1"), $("#case5"), $("#case9"))
-              || verifier($("#case2"), $("#case5"), $("#case7")) ;
+          return verifier($("#case1")=(0), $("#case2")=(1), $("#case3")=(2))
+              || verifier($("#case4")=(3), $("#case5")=(4), $("#case6")=(5))
+              || verifier($("#case7")=(6), $("#case8")=(7), $("#case9")=(8))
+              || verifier($("#case1")=(0), $("#case4")=(3), $("#case7")=(6))
+              || verifier($("#case2")=(1), $("#case5")=(4), $("#case8")=(7))
+              || verifier($("#case3")=(2), $("#case6")=(5), $("#case9")=(8))
+              || verifier($("#case1")=(0), $("#case5")=(4), $("#case9")=(8))
+              || verifier($("#case2")=(1), $("#case5")=(4), $("#case7")=(6)) ;
 
       }
 
@@ -201,7 +149,7 @@ $(".case").on("click",function donneImage(){
               window.alert('Partie terminée.\nRechargez la page pour rejouer.') ;
               return ;
           }
-          if (document.images[n].src.search('fond-blanc.jpg') != -1)
+          if (document.images[n].src.search('images/fond-blanc.jpg') != -1)
           {
 
               document.images[n].src = donneImage(coups).src ;
